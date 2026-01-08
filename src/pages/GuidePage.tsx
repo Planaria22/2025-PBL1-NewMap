8a54ce2b6fb2787524a90e255fc8ad507b3525c4
Planarian33 <rt23153i@st.omu.ac.jp>
2026-01-08 13:38:17 +0900

    create pages

---
+++ src/pages/GuidePage.tsx
@@ -0,0 +1,10 @@
+import Title from "../components/elements/title";
+
+const GuidePage = () => {
+    return(
+        <div className="flex flex-col w-screen items-center justify-center">
+            <Title>地図の見方</Title>
+        </div>
+    )
+}
+export default GuidePage;
---
+++ src/pages/MapPage.tsx
@@ -0,0 +1,8 @@
+import Title from "../components/elements/title";
+
+const MapPage = () => {
+    return (
+        <Title>地図</Title>
+    )
+}
+export default MapPage;
