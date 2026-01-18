d3e3194c6855902c3978e27c17467d092f618a41
Planarian33 <rt23153i@st.omu.ac.jp>
2026-01-19 00:27:41 +0900

    fix header

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="fixed flex items-center justify-between top-0 left-0 right-0 z-50 h-16 pl-2 bg-darkColor">
       <h1>
-        <Link to="/" className="text-2xl text-white">
+        <Link to="/" className="text-base md:text-4xl text-white hover:text-white">
           大阪公大高専バリアフリーマップ
         </Link>
       </h1>
