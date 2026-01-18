94b1a5067703dfbba32fb7ce1bf06dcdd00a2817
Planarian33 <rt23153i@st.omu.ac.jp>
2026-01-18 20:44:06 +0900

    fix approutes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,5 +1,4 @@
-// import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import TopPage from "./pages/TopPage"
 import GuidePage from "./pages/GuidePage"
 import MapPage from "./pages/MapPage"
@@ -7,14 +6,14 @@ import AboutPage from "./pages/AboutPage"
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<TopPage />} />
-        <Route path="/guide" element={<GuidePage />}/>
-        <Route path="/map" element={<MapPage />}/>
-        <Route path="/about" element={<AboutPage />}/>
+        <Route path="/guide" element={<GuidePage />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
